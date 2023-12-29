7d0f479c02f0fd5f10c0012752605da80255ff8a
Lucas Detling <lucas.detling@gmail.com>
2023-12-29 12:51:14 +0100

    delete comment

--- PHP-blog-emilie-forteroche/controllers/CommentController.php
+++ PHP-blog-emilie-forteroche/controllers/CommentController.php
@@ -1,12 +1,12 @@
 <?php
 
-class CommentController 
+class CommentController
 {
     /**
      * Ajoute un commentaire.
      * @return void
      */
-    public function addComment() : void
+    public function addComment(): void
     {
         // Récupération des données du formulaire.
         $pseudo = Utils::request("pseudo");
@@ -42,6 +42,26 @@ class CommentController
         }
 
         // On redirige vers la page de l'article.
+        Utils::redirect("showArticle", ['id' => $idArticle]);
+    }
+
+    public function deleteComment(): void
+    {
+        $idArticle = Utils::request("idArticle");
+        $idComment = Utils::request("idComment");
+
+        if (empty($idArticle) || empty($idComment)) {
+            throw new Exception("Tous les champs sont obligatoires");
+        }
+
+        $commentManager = new CommentManager();
+        $comment = $commentManager->getCommentById($idComment);
+        $delete = $commentManager->deleteComment($comment);
+
+        if (!$delete) {
+            throw new Exception("Une erreur est survenue lors de la suppression du commentaire.");
+        }
+
         Utils::redirect("showArticle", ['id' => $idArticle]);
     }
 }
